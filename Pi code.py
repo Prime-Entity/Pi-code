f480e83fea57e72bbf37a2b1430565a1f937bcdb
Prime-Entity <mrshadowjr101@gmail.com>
2025-03-15 12:44:49 +0000

    Add file

---
+++ Pi code.py
@@ -0,0 +1,165 @@
+import keyboard
+import time
+import sys
+import io
+
+sys=stdin = io.StringIO()
+
+while True:
+    if keyboard.is_pressed('a'):
+        print("awful ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed ('A'):
+        print ("awful ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('b'):
+        print ("brat ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('B'):
+        print ("brat ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('c'):
+        print ("creep ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('C'):
+        print ("creep ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('d'):
+        print ("disapiontment ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('D'):
+        print ("disapiontment ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('e'):
+        print ("el bozo ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('E'):
+        print ("el bozo ", end =" ")
+        time.sleep(0.4)  
+    elif keyboard.is_pressed('f'):
+        print ("fart ", end =" ")
+        time.sleep(0.4)  
+    elif keyboard.is_pressed('F'):
+        print ("fart ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('g'):
+        print ("grundge ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('G'):
+        print ("grundge ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('h'):
+        print ("horrible ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('H'):
+        print ("horrible ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('i'):
+        print ("idiot ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('I'):
+        print ("idiot ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('j'):
+        print ("jerk ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('J'):
+        print ("jerk ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('k'):
+        print ("knuckle head ", end =" ")
+        time.sleep(0.4) 
+    elif keyboard.is_pressed('K'):
+        print ("knuckle head ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('l'):
+        print ("loon ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('L'):
+        print ("loon ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('m'):
+        print ("moron ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('M'):
+        print ("moron ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('n'):
+        print ("nerd ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('N'):
+        print ("nerd ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('o'):
+        print ("oblivous twit ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('O'):
+        print ("oblivous twit ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('p'):
+        print ("pathetic excuse ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('p'):
+        print ("pathetic excuse ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('q'):
+        print ("questional fool ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('Q'):
+        print ("questional fool ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('r'):
+        print ("ridiculous imbicile ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('R'):
+        print ("ridiculous imbicile ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('s'):
+        print ("stupid monkey ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('S'):
+        print ("stupid monkey ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('t'):
+        print ("tawdry ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('T'):
+        print ("tawdry ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('u'):
+        print ("useless dolt ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('U'):
+        print ("useless dolt ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('v'):
+        print ("vain ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('V'):
+        print ("vain ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('w'):
+        print ("worthless", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('W'):
+        print ("worthless ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('x'):
+        print ("xenophobic fool", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('X'):
+        print ("xenophobic fool ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('y'):
+        print ("you absolute dount ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('Y'):
+        print ("you ablosute dount ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('z'):
+        print ("zero IQ ", end =" ")
+        time.sleep(0.4)
+    elif keyboard.is_pressed('Z'):
+        print ("zero IQ ", end =" ")
+        time.sleep(0.4)
+
